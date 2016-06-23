c83275c7b678536d99f7f237a030be0ffc50c29c
MichalStachnik <stachnikmichal@gmail.com>
2016-06-23 18:42:52 +0000

    Add files via upload

--- nodeEventEmitter/app.js
+++ nodeEventEmitter/app.js
@@ -1,1 +1,15 @@
+var Emitter = require('events');
+var eventConfig = require('./config').events;
 
+var emtr = new Emitter();
+
+emtr.on(eventConfig.GREET, function(){
+    console.log('hey from the first one');
+});
+
+emtr.on(eventConfig.GREET, function(){
+    console.log('hey from the second one');
+});
+
+console.log('hello');
+emtr.emit(eventConfig.GREET);
