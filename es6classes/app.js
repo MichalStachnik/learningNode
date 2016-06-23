27e656c14d0181127ca100cc55d5c61f9a0ba870
MichalStachnik <stachnikmichal@gmail.com>
2016-06-23 18:50:01 +0000

    Add files via upload

--- es6classes/app.js
+++ es6classes/app.js
@@ -1,1 +1,19 @@
+'use strict';
 
+class person{
+    constructor(firstname, lastname){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    
+    greet(){
+        console.log('hello ' + this.firstName + ' ' + this.lastName);
+    }
+}
+
+//creating an object of the person prototype
+var michal = new person('michal', 's');
+michal.greet();
+
+var maura = new person('maura', 's');
+maura.greet();
