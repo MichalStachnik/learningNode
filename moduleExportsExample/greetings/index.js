923b6bf15ac7829e583196d352505a3deb7f5521
MichalStachnik <stachnikmichal@gmail.com>
2016-06-23 18:47:58 +0000

    Add files via upload

---
+++ moduleExportsExample/greetings/index.js
@@ -0,0 +1,8 @@
+var english = require('./english');
+var spanish = require('./spanish');
+
+module.exports = {
+            //refers to the variables above ^^
+    english: english,
+    spanish: spanish
+};
